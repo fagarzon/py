85f0e2b7c941f79f48fb216b07a498bb6665c8e1
Felix Andres Garzon <felix.garzon@quironsalud.com>
2022-01-24 09:09:02 -0500

    Inicio conversor

---
+++ Conversor.py
@@ -0,0 +1,31 @@
+def conversor(tipo_peso, valor_dolar):
+    pesos = input("Cuantos pesos " + tipo_peso +  " tienes?:  ")
+    pesos = float(pesos)
+    valor_dolar = valor_dolar
+    dolares = pesos/valor_dolar
+    dolares = round(dolares, 2)
+    dolares = str(dolares)
+    print("Tienes $" + dolares + " Dolares")    
+
+menu = """
+Bienvenido al conversor
+
+1. Pesos colombianos
+2. Pesos argentinos
+3. Pesos Mexicanos
+
+Escoger una Opción
+"""
+opcion = input(menu)
+
+if opcion == "1":
+    conversor("Colombianos" , 3875)
+elif opcion == "2":
+    conversor("Argentinos" , 65)
+elif opcion == "3":
+    conversor("Mexicanos" , 24)
+else:
+    print("opcion incorrecta")
+
+
+
