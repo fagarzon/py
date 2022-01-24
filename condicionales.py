d79c469cff1379d8b8eb51b732c9c16ea04ede47
fagarzon <fagarzon77@gmail.com>
2022-01-24 09:17:49 -0500

    1 commit

---
+++ condicionales.py
@@ -0,0 +1,17 @@
+from ast import Pass
+
+
+# edad = int(input("Escribe tu edad: "))
+# if edad > 18: 
+#     print("Mayor de Edad ")
+# else:
+#     print("Menor de Edad ")
+
+numero = int(input("Escribe un numero: "))
+if numero > 5:
+    print("Mayor a 5 ")
+elif numero == 5:
+    print("Igual a 5 ")
+else:
+    print("Menor a 5 ")
+
---
+++ funciones.py
@@ -0,0 +1,30 @@
+#def imprimir_mensaje():
+#    print("Mensaje Especial")
+#    print("Aprendiendio a usar funciones")
+
+#imprimir_mensaje()
+
+from unittest import result
+
+
+def conversacion(mensaje):
+    print("Hola")
+    print(mensaje)
+
+#opcion = int(input("Elige una opcion (1 ,2 ,3):"))
+#if opcion == 1:
+#    conversacion("Elegiste la opcion numero 1")
+#elif opcion == 2:
+#    conversacion("Elegiste la opcion numero 2")
+#elif opcion == 3:
+#    conversacion("Elegiste la opcion numero 3")
+    
+def suma(a , b):
+    print("se suman dos numero")
+    resultado = a + b
+    return resultado
+
+sumatoria = suma(5,3)
+print(sumatoria)
+
+
